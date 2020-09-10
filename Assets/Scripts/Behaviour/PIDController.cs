0f383c2d44dae1fa9bf1e8193cf4083c0bd233f5
Francis Tremblay Pelletier <francis-tremblay@hotmail.ca>
2020-09-10 15:30:32 -0400

    Physics movement and collider prog

---
+++ Assets/Scripts/Behaviour/PIDController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PIDController : MonoBehaviour
+{
+	public float pFactor, iFactor, dFactor;
+
+	private float integral;
+	private float lastError;
+
+
+	public PIDController(float pFactor, float iFactor, float dFactor) {
+		this.pFactor = pFactor;
+		this.iFactor = iFactor;
+		this.dFactor = dFactor;
+	}
+
+
+	public float updatePid(float setpoint, float actual, float timeFrame) {
+		float present = setpoint - actual;
+		integral += present * timeFrame;
+		float deriv = (present - lastError) / timeFrame;
+		lastError = present;
+		return present * pFactor + integral * iFactor + deriv * dFactor;
+	}
+
+	public float updatePid(float setpoint, float actual, float timeFrame, float pFactor, float iFactor, float dFactor) {
+		float present = setpoint - actual;
+		integral += present * timeFrame;
+		float deriv = (present - lastError) / timeFrame;
+		lastError = present;
+		return (present * pFactor) + (integral * iFactor) + (deriv * dFactor);
+	}
+}
---
+++ Assets/Scripts/Behaviour/RigidFollowRigid.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RigidFollowRigid : MonoBehaviour {
+
+    public Transform parent; //  object we want to follow
+
+    private Vector3 parentPositionOffset = Vector3.zero; // this is the offset of the parent, to keep correct positioning
+
+    private Rigidbody thisRigidbody = null;
+    public int strenght = 15;
+    private PIDController pidControllerX = null;
+    private PIDController pidControllerY = null;
+    private PIDController pidControllerZ = null;
+
+    public float pFactor = 60;
+    public float iFactor = 0.05f;
+    public float dFactor = 1f;
+
+    private float biggestDistance = 0.0f;
+
+    // Start is called before the first frame update
+    void Start() {
+        thisRigidbody = this.GetComponent<Rigidbody>();
+
+        parentPositionOffset = new Vector3(parent.position.x - transform.position.x, parent.position.y - transform.position.y, parent.position.z - transform.position.z);
+
+        pidControllerX = new PIDController(pFactor, iFactor, dFactor);
+        pidControllerY = new PIDController(pFactor, iFactor, dFactor);
+        pidControllerZ = new PIDController(pFactor, iFactor, dFactor);
+    }
+
+    void FixedUpdate() {
+        if (parent != null) {
+            //float currentDist = Vector3.Distance((parent.transform.position + parentPositionOffset), this.transform.position);
+
+            //
+            float pidX = pidControllerX.updatePid((parent.position.x - parentPositionOffset.x), this.transform.position.x, Time.fixedDeltaTime, pFactor, iFactor, dFactor);
+            float pidY = pidControllerY.updatePid((parent.position.y - parentPositionOffset.y), this.transform.position.y, Time.fixedDeltaTime, pFactor, iFactor, dFactor);
+            float pidZ = pidControllerZ.updatePid((parent.position.z - parentPositionOffset.z), this.transform.position.z, Time.fixedDeltaTime, pFactor, iFactor, dFactor);
+            Vector3 newVector = new Vector3(pidX, pidY, pidZ);
+
+            Vector3 newVelocity = newVector;//.normalized * ((float)(strenght));
+            newVelocity.y = this.thisRigidbody.velocity.y;
+
+            thisRigidbody.velocity = newVelocity;
+
+
+            // debug
+           // if (((parent.transform.position - parentPositionOffset) - this.transform.position).magnitude > biggestDistance && ((parent.transform.position - parentPositionOffset) - this.transform.position).magnitude < 1.5f) {
+                biggestDistance = ((parent.position - parentPositionOffset) - this.transform.position).magnitude;
+                Debug.Log("Biggest distance: " + biggestDistance);
+           // }
+        }
+    }
+}
