fc0fb88d151580e9a56ae44e2e28260c6a62076e
Francis Tremblay Pelletier <francis-tremblay@hotmail.ca>
2020-09-10 15:29:56 -0400

    Progress on adding physics movement and collisions

---
+++ Assets/Scripts/Player/PlayerCapsule.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCapsule : MonoBehaviour {
+
+    public Transform playerHeadTransform = null;
+    public CapsuleCollider capsuleCollider = null;
+
+    private Rigidbody rigidbody = null;
+
+    // Start is called before the first frame update
+    void Start() {
+
+        //capsuleCollider = GetComponent<CapsuleCollider>();
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // called once per physics update
+    void FixedUpdate() {
+        // make the heigh the same as headset + radius
+        capsuleCollider.height = playerHeadTransform.localPosition.y;
+        capsuleCollider.center = new Vector3(0, capsuleCollider.height / 2, 0);
+
+        // make capsule follow head on x,z axis
+    }
+}
