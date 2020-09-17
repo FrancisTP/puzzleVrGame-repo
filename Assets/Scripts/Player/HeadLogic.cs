e3d8eec596115d12d97999d0a12b0f182bc3d319
Francis Tremblay Pelletier <francis-tremblay@hotmail.ca>
2020-09-17 15:30:10 -0400

    Work in progress

---
+++ Assets/Scripts/Player/HeadLogic.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeadLogic : MonoBehaviour {
+
+    public Transform headCamera = null;
+    public Transform body = null;
+    private Rigidbody rigidbody = null;
+    private Transform transform = null;
+
+    void Start() {
+
+        //capsuleCollider = GetComponent<CapsuleCollider>();
+        rigidbody = GetComponent<Rigidbody>();
+        transform = GetComponent<Transform>();
+    }
+
+    // called once per physics update
+    void FixedUpdate() {
+        // make head go directly on MainCamera, if collision occurs set on collision body
+        transform.position = headCamera.position;
+    }
+}
